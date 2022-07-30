e0db396376c0b6d2bb2f77030896692adcee0542
mustafa.sacli <mustasacli@hotmail.com>
2022-07-30 17:36:20 +0300

    DbConnectionTypes class comments arranged.

--- Simply.Soln/src_simply/Simply.Data/Enums/DbConnectionTypes.cs
+++ Simply.Soln/src_simply/Simply.Data/Enums/DbConnectionTypes.cs
@@ -6,11 +6,13 @@
 
 namespace Simply.Data.Enums
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// <summary>   Values that represent Database connection types. </summary>
+    /// <summary>
+    /// Values that represent Database connection types.
+    /// </summary>
     ///
-    /// <remarks>   Mustafa SAÇLI, 4.05.2019. </remarks>
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <remarks>
+    /// Mustafa SAÇLI, 4.05.2019.
+    /// </remarks>
     public enum DbConnectionTypes : byte
     {
         /// <summary>
@@ -83,52 +85,83 @@ namespace Simply.Data.Enums
         /// </summary>
         Synergy = 113,
 
-        /// <summary>   An enum constant representing the MsSql odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the MsSql odbc option.
+        /// </summary>
         SqlOdbc = 114,
 
-        /// <summary>   An enum constant representing the MsSql oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the MsSql oledb option.
+        /// </summary>
         SqlOledb = 115,
 
-        /// <summary>   An enum constant representing the Oracle odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the Oracle odbc option.
+        /// </summary>
         OracleOdbc = 116,
 
-        /// <summary>   An enum constant representing the Oracle oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the Oracle oledb option.
+        /// </summary>
         OracleOledb = 117,
 
-        /// <summary>   An enum constant representing the PostgreSql odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the PostgreSql odbc option.
+        /// </summary>
         PostgreSqlOdbc = 118,
 
-        /// <summary>   An enum constant representing the PostgreSql oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the PostgreSql oledb option.
+        /// </summary>
         PostgreSqlOledb = 119,
 
-        /// <summary>   An enum constant representing the DB2 odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the DB2 odbc option.
+        /// </summary>
         DB2Odbc = 120,
 
-        /// <summary>   An enum constant representing the DB2 oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the DB2 oledb option.
+        /// </summary>
         DB2Oledb = 121,
 
-        /// <summary>   An enum constant representing the MySql odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the MySql odbc option.
+        /// </summary>
         MySqlOdbc = 122,
 
-        /// <summary>   An enum constant representing the MySql oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the MySql oledb option.
+        /// </summary>
         MySqlOledb = 123,
 
-        /// <summary>   An enum constant representing the Firebird odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the Firebird odbc option.
+        /// </summary>
         FirebirdOdbc = 124,
 
-        /// <summary>   An enum constant representing the Firebird oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the Firebird oledb option. </summary>
         FirebirdOledb = 125,
 
-        /// <summary>   An enum constant representing the SQL base odbc option. </summary>
+        /// <summary>
+        /// An enum constant representing the SQL base odbc option.
+        /// </summary>
         SqlBaseOdbc = 126,
 
-        /// <summary>   An enum constant representing the SQL base oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the SQL base oledb option.
+        /// </summary>
         SqlBaseOledb = 127,
 
-        /// <summary>   An enum constant representing the synergy ODBC option. </summary>
+        /// <summary>
+        /// An enum constant representing the synergy ODBC option.
+        /// </summary>
         SynergyOdbc = 128,
 
-        /// <summary>   An enum constant representing the synergy oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the synergy oledb option.
+        /// </summary>
         SynergyOledb = 129,
 
         /// <summary>
@@ -147,10 +180,14 @@ namespace Simply.Data.Enums
         /// </summary>
         SybaseASA = 132,
 
-        /// <summary>   An enum constant representing the Sybase ASA ODBC option. </summary>
+        /// <summary>
+        /// An enum constant representing the Sybase ASA ODBC option.
+        /// </summary>
         SybaseASAOdbc = 133,
 
-        /// <summary>   An enum constant representing the Sybase ASA oledb option. </summary>
+        /// <summary>
+        /// An enum constant representing the Sybase ASA oledb option.
+        /// </summary>
         SybaseASAOledb = 134,
     };
 }
