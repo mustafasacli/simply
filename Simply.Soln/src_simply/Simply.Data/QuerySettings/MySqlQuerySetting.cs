9523fcd61b764cb2d764da3dda4646d21fb33f40
mustafa.sacli <mustasacli@hotmail.com>
2022-08-10 01:00:02 +0300

    mysql query settings.

--- Simply.Soln/src_simply/Simply.Data/QuerySettings/MySqlQuerySetting.cs
+++ Simply.Soln/src_simply/Simply.Data/QuerySettings/MySqlQuerySetting.cs
@@ -21,8 +21,8 @@ namespace Simply.Data
             this.Prefix = BackquoteString;// "`";
             this.Suffix = BackquoteString;// "`";
             this.StringConcatOperation = "CONCAT( #params# )";
-            this.SkipAndTakeFormat = "SELECT * FROM (#SQL_SCRIPT#) LIMIT #TAKE#, #SKIP# ";
-            this.LastFormat = "SELECT * FROM (#SQL_SCRIPT#) LIMIT #TAKE#, #SKIP# ";
+            this.SkipAndTakeFormat = "SELECT EXT1.* FROM (#SQL_SCRIPT#) AS EXT1 LIMIT #SKIP#, #TAKE#";
+            this.LastFormat = "SELECT EXT2.* FROM (SELECT EXT1.*, ROW_NUMBER() OVER() AS RN1 FROM ( #SQL_SCRIPT# ) AS EXT1) AS EXT2 ORDER BY EXT2.RN1 DESC LIMIT 0, 1 ";
             this.SubstringFormat = "SUBSTR(#0#, #1#, #2#)";
             this.CountFormat = "SELECT COUNT(*) FROM ( #SQL_SCRIPT# ) AS CNT";
         }
