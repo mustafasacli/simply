dbb1af6d67ae26ee2ca2b6a0933ca278c9b84f3f
mustafasacli <mustasacli@hotmail.com>
2022-09-16 18:39:38 +0300

    BaseQuerySetting developments.

---
+++ Simply.Soln/src_simply/Simply.Data/QuerySettings/Core/BaseQuerySetting.cs
@@ -0,0 +1,83 @@
+﻿using Simply.Data.Enums;
+using Simply.Data.Interfaces;
+
+namespace Simply.Data
+{
+    /// <summary>
+    /// Represents Parameter Prefix, Column-Table Prefix And Suffix and last IdentityInsert Part.
+    /// </summary>
+    public abstract class BaseQuerySetting : IQuerySetting
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseQuerySetting"/> class.
+        /// </summary>
+        /// <param name="connectionType">Database Connection type.</param>
+        protected BaseQuerySetting(DbConnectionTypes connectionType)
+        {
+            this.ConnectionType = connectionType;
+        }
+
+        /// <summary>
+        /// Gets Parameter Name Prefix.
+        /// </summary>
+        public string ParameterPrefix
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets Table and Column Name Prefix.
+        /// </summary> 
+        public string Prefix
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets Table and Column Name Suffix.
+        /// </summary>
+        public string Suffix
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets the string concat operation.
+        /// </summary>
+        /// <value>
+        /// The string concat operation.
+        /// </value>
+        public string StringConcatOperation
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets the Skip and take format.
+        /// </summary>
+        public string SkipAndTakeFormat
+        { get; protected set; }
+
+        /// <summary>
+        /// Last Record Sql Format
+        /// </summary>
+        public string LastFormat
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets Db Connection Type.
+        /// </summary>
+        public DbConnectionTypes ConnectionType
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets Parameter Suffix.
+        /// </summary>
+        public string ParameterSuffix
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets the string substring format.
+        /// </summary>
+        public string SubstringFormat
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets the string count format.
+        /// </summary>
+        public string CountFormat
+        { get; protected set; }
+    }
+}
--- Simply.Soln/src_simply/Simply.Data/QuerySettings/MySqlQuerySetting.cs
+++ Simply.Soln/src_simply/Simply.Data/QuerySettings/MySqlQuerySetting.cs
@@ -1,5 +1,4 @@
 ﻿using Simply.Data.Enums;
-using Simply.Data.Interfaces;
 using static Simply.Data.Constants.InternalAppValues;
 
 namespace Simply.Data
@@ -7,13 +6,13 @@ namespace Simply.Data
     /// <summary>
     /// Represents Parameter Prefix, Column-Table Prefix And Suffix and last IdentityInsert Part.
     /// </summary>
-    internal class MySqlQuerySetting : IQuerySetting
+    internal class MySqlQuerySetting : BaseQuerySetting
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MySqlQuerySetting"/> class.
         /// </summary>
         /// <param name="connectionType">Database Connection type.</param>
-        public MySqlQuerySetting(DbConnectionTypes connectionType)
+        public MySqlQuerySetting(DbConnectionTypes connectionType) : base(connectionType)
         {
             this.ConnectionType = connectionType;
             this.ParameterPrefix = AtChar.ToString();
@@ -26,66 +25,5 @@ namespace Simply.Data
             this.SubstringFormat = "SUBSTR(#0#, #1#, #2#)";
             this.CountFormat = "SELECT COUNT(*) FROM ( #SQL_SCRIPT# ) AS CNT";
         }
-
-        /// <summary>
-        /// Gets Parameter Name Prefix.
-        /// </summary>
-        public string ParameterPrefix
-        { get; private set; }
-
-        /// <summary>
-        /// Gets Table and Column Name Prefix.
-        /// </summary>
-        public string Prefix
-        { get; private set; }
-
-        /// <summary>
-        /// Gets Table and Column Name Suffix.
-        /// </summary>
-        public string Suffix
-        { get; private set; }
-
-        /// <summary>
-        /// Gets the string concat operation.
-        /// </summary>
-        /// <value>The string concat operation.</value>
-        public string StringConcatOperation
-        { get; private set; }
-
-        /// <summary>
-        /// Gets the Skip and take format.
-        /// </summary>
-        public string SkipAndTakeFormat
-        { get; private set; }
-
-        /// <summary>
-        /// Last Record Sql Format
-        /// </summary>
-        public string LastFormat
-        { get; private set; }
-
-        /// <summary>
-        /// Gets Db Connection Type.
-        /// </summary>
-        public DbConnectionTypes ConnectionType
-        { get; private set; }
-
-        /// <summary>
-        /// Gets Parameter Suffix.
-        /// </summary>
-        public string ParameterSuffix
-        { get; private set; }
-
-        /// <summary>
-        /// Gets the string substring format.
-        /// </summary>
-        public string SubstringFormat
-        { get; private set; }
-
-        /// <summary>
-        /// Gets the string count format.
-        /// </summary>
-        public string CountFormat
-        { get; private set; }
     }
 }
