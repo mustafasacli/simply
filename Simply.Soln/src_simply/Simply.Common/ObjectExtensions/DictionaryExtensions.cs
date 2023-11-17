50fc2fa7b2b39c71016d65193797fe55bfee7545
mustafa.sacli <mustasacli@hotmail.com>
2023-11-17 09:51:32 +0300

    dictionary to xml and json methods added to DictionaryExtensions.

--- Simply.Soln/src_simply/Simply.Common/ObjectExtensions/DictionaryExtensions.cs
+++ Simply.Soln/src_simply/Simply.Common/ObjectExtensions/DictionaryExtensions.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Simply.Common.Objects;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Simply.Common
 {
@@ -32,6 +35,127 @@ namespace Simply.Common
                 value = defaultValue;
 
             return value;
+        }
+
+        /// <summary>
+        /// Tos the json string.
+        /// </summary>
+        /// <param name="keyValuePairs">The key value pairs.</param>
+        /// <param name="formatSetting">Format settings.</param>
+        /// <returns>A string.</returns>
+        public static string ToJsonString(this IDictionary<string, object> keyValuePairs,
+            SimpleFormatSetting formatSetting)
+        {
+            if (keyValuePairs == null)
+                return string.Empty;
+
+            if (keyValuePairs.Count == 0)
+                return string.Empty;
+
+            formatSetting = formatSetting ?? SimpleFormatSetting.New();
+
+            string nullString = "null";
+            string tabString = "\t";
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = keyValuePairs.Count;
+            int counter = 0;
+
+            foreach (string key in keyValuePairs.Keys)
+            {
+                if (formatSetting.AddTab)
+                    stringBuilder.Append(tabString);
+
+                stringBuilder.AppendFormat("\"{0}\" : ", key);
+                object value = keyValuePairs[key];
+
+                if (value.IsNullOrDbNull())
+                {
+                    stringBuilder.Append(nullString);
+                }
+                else
+                {
+                    if (value is string str)
+                    {
+                        stringBuilder.Append("\"" + str + "\"");
+                    }
+                    else if (value is DateTime date)
+                    {
+                        // "yyyyy-MM-dd HH:mm:ss.fffffff"
+                        stringBuilder.Append("\"" + date.ToString(formatSetting.DatetimeFormat) + "\"");
+                    }
+                    else
+                    {
+                        stringBuilder.Append(value.ToString());
+                    }
+                }
+                if (counter != count - 1)
+                    stringBuilder.Append(",");
+
+                if (formatSetting.AddNewLine)
+                    stringBuilder.AppendLine();
+            }
+
+            string tempValue = stringBuilder.ToString();
+            string result = string.Concat("{", formatSetting.AddNewLine ? Environment.NewLine : string.Empty, tempValue, "}");
+            return result;
+        }
+
+        /// <summary>
+        /// Tos the xml string.
+        /// </summary>
+        /// <param name="keyValuePairs">The key value pairs.</param>
+        /// <param name="mainRowName">The main row name.</param>
+        /// <param name="formatSetting">Format settings.</param>
+        /// <returns>A string.</returns>
+        public static string ToXmlString(this IDictionary<string, object> keyValuePairs,
+            SimpleFormatSetting formatSetting)
+        {
+            if (formatSetting.MainXmlNodeName.IsNullOrSpace())
+                throw new ArgumentNullException(nameof(SimpleFormatSetting.MainXmlNodeName));
+
+            if (!(keyValuePairs?.Any() ?? false))
+                return string.Format("<{0}></{0}>", formatSetting.MainXmlNodeName);
+
+            formatSetting = formatSetting ?? SimpleFormatSetting.New();
+
+            string tabString = "\t";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (string key in keyValuePairs.Keys)
+            {
+                if (formatSetting.AddNewLine)
+                    stringBuilder.AppendLine();
+
+                if (formatSetting.AddTab)
+                    stringBuilder.Append(tabString);
+
+                object value = keyValuePairs[key];
+
+                if (value.IsNullOrDbNull())
+                {
+                    stringBuilder.AppendFormat("<{0} nil=\"true\"></{0}>", key);
+                }
+                else
+                {
+                    if (value is string str)
+                    {
+                        stringBuilder.AppendFormat("<{0}>{1}</{0}>", key, str);
+                    }
+                    else if (value is DateTime date)
+                    {
+                        // "yyyyy-MM-dd HH:mm:ss.fffffff"
+                        stringBuilder.AppendFormat("<{0}>{1}</{0}>", key, date.ToString(formatSetting.DatetimeFormat));
+                    }
+                    else
+                    {
+                        stringBuilder.AppendFormat("<{0}>{1}</{0}>", key, value.ToString());
+                    }
+                }
+            }
+
+            string tempValue = stringBuilder.ToString();
+            string result = string.Format("<{0}>{1}</{0}>", formatSetting.MainXmlNodeName, tempValue);
+            return result;
         }
     }
 }
---
+++ Simply.Soln/src_simply/Simply.Common/Objects/SimpleFormatSetting.cs
@@ -0,0 +1,104 @@
+﻿namespace Simply.Common.Objects
+{
+    /// <summary>
+    /// The simple format setting.
+    /// </summary>
+    public class SimpleFormatSetting
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleFormatSetting"/> class.
+        /// </summary>
+        public SimpleFormatSetting()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleFormatSetting"/> class.
+        /// </summary>
+        /// <param name="addTab">If true, add tab.</param>
+        /// <param name="addNewLine">If true, add new line.</param>
+        /// <param name="datetimeFormat">The datetime format.</param>
+        public SimpleFormatSetting(bool addTab = true, bool addNewLine = true,
+            string datetimeFormat = "yyyyy-MM-dd HH:mm:ss.fffffff")
+        {
+            this.AddTab = addTab;
+            this.AddNewLine = addNewLine;
+            this.DatetimeFormat = datetimeFormat;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether add tab.
+        /// </summary>
+        public bool AddTab
+        { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether add new line.
+        /// </summary>
+        public bool AddNewLine
+        { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the datetime format.
+        /// </summary>
+        public string DatetimeFormat
+        { get; set; } = "yyyyy-MM-dd HH:mm:ss.fffffff";
+
+        /// <summary>
+        /// Gets or sets the main xml node name. it is used for xml formatting.
+        /// </summary>
+        public string MainXmlNodeName
+        { get; set; } = "row";
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <returns>A SimpleFormatSetting.</returns>
+        public static SimpleFormatSetting New()
+        { return new SimpleFormatSetting(); }
+
+        /// <summary>
+        /// Sets the new line and returns current instance.
+        /// </summary>
+        /// <param name="addNewLine">If true, add new line.</param>
+        /// <returns>A SimpleFormatSetting.</returns>
+        public SimpleFormatSetting SetNewLine(bool addNewLine)
+        {
+            this.AddNewLine = addNewLine;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the tab and returns current instance.
+        /// </summary>
+        /// <param name="addTab">If true, add tab.</param>
+        /// <returns>A SimpleFormatSetting.</returns>
+        public SimpleFormatSetting SetTab(bool addTab)
+        {
+            this.AddTab = addTab;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the datetime format and returns current instance.
+        /// </summary>
+        /// <param name="datetimeFormat">The datetime format.</param>
+        /// <returns>A SimpleFormatSetting.</returns>
+        public SimpleFormatSetting SetDatetimeFormat(string datetimeFormat)
+        {
+            this.DatetimeFormat = datetimeFormat;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the main xml node name and returns current instance.
+        /// </summary>
+        /// <param name="mainXmlNodeName">The main xml node name.</param>
+        /// <returns>A SimpleFormatSetting.</returns>
+        public SimpleFormatSetting SetMainXmlNodeName(string mainXmlNodeName)
+        {
+            this.MainXmlNodeName = mainXmlNodeName;
+            return this;
+        }
+    }
+}
